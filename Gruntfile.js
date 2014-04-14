b54f510afd54466eaed97ac4dd54f013e30b14d0
gpspake <gpspake@gmail.com>
2014-04-13 22:16:21 -0500

    Add compass
    Added compass support. Updated gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,66 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed'
+        sass: {
+            options: {
+                includePaths: ['bower_components/foundation/scss']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss',
+                    'css/style.css': 'scss/style.scss',
+                    'css/uthsc.css': 'scss/uthsc.scss'
+                }
+            }
         },
-        files: {
-          'css/app.css': 'scss/app.scss',
-          'css/style.css': 'scss/style.scss'
-        }        
-      }
-    },
 
-	  uglify: {
-	    options: {
-	      mangle: false
-	    },
-	    my_target: {
-	      files: {
-	        'js/modernizr.min.js': ['bower_components/modernizr/modernizr.js']
-	      }
-	    }
-	  },
+        compass: {                  // Task
+            dist: {                   // Target
+                options: {              // Target options
+                    sassDir: 'scss',
+                    cssDir: 'css'
+                }
+            }
+        },
 
-		copy: {
-		  main: {
-		    nonull: true,
-		    src: 'bower_components/foundation/js/foundation.min.js',
-		    dest: 'js/foundation.min.js',
-		  },
-		},
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'js/modernizr.min.js': ['bower_components/modernizr/modernizr.js']
+                }
+            }
+        },
 
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
+        copy: {
+            main: {
+                nonull: true,
+                src: 'bower_components/foundation/js/foundation.min.js',
+                dest: 'js/foundation.min.js'
+            }
+        },
 
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
+        watch: {
+            grunt: { files: ['Gruntfile.js'] },
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  
-  grunt.registerTask('build', ['sass','uglify','copy']);
-  grunt.registerTask('default', ['build','watch']);
+            compass: {
+                files: 'scss/**/*.scss',
+                tasks: ['compass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compass')
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('build', ['uglify','copy']);
+    grunt.registerTask('default', ['build','compass','watch']);
 }

29f9dc5811592fd88d8b3c20a6c90f489bd34419
gpspake <gpspake@gmail.com>
2014-04-14 14:17:10 -0500

    Remove entry-author from single template

--- single.php
+++ single.php
@@ -19,17 +19,17 @@
 				<?php edit_post_link('Edit this Post'); ?>
 			</footer>
 		</article>
-		<div class="entry-author panel">
+		<!--<div class="entry-author panel">
 			<div class="row">
 				<div class="large-3 columns">
-					<?php echo get_avatar( get_the_author_meta('user_email'), 95 ); ?>
+					<?php /*echo get_avatar( get_the_author_meta('user_email'), 95 ); */?>
 				</div>
 				<div class="large-9 columns">
-					<h4><?php the_author_posts_link(); ?></h4>
-					<p class="cover-description"><?php the_author_meta('description'); ?></p>
+					<h4><?php /*the_author_posts_link(); */?></h4>
+					<p class="cover-description"><?php /*the_author_meta('description'); */?></p>
 				</div>
 			</div>
-		</div>
+		</div>-->
 		<?php comments_template(); ?>
 	<?php endwhile; // End the loop ?>
 

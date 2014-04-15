34a4ef9284cb909e5dcb78aa7826e88a3664af5f
gpspake <gpspake@gmail.com>
2014-04-14 21:17:59 -0500

    Style homepage

--- content.php
+++ content.php
@@ -16,7 +16,7 @@
 		<div class="row">
             <div class="medium-3 hide-for-small columns"><figure class="th"><a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {the_post_thumbnail('large'); } ?></a></figure></div>
             <div class="medium-9 columns"><?php the_excerpt(); ?></div>
-
         </div>
-	</div>
+        <p class="entry-tags"><?php the_tags(); ?></p>
+    </div>
 </article>

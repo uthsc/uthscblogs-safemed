73a8bb0b2e655acab03a02395c993def9ce159a8
George Spake <gpspake@gmail.com>
2014-04-15 02:09:51 -0500

    Fix Missing thumb on archive pages

--- content.php
+++ content.php
@@ -14,8 +14,13 @@
 	</header>
 	<div class="entry-content">
 		<div class="row">
-            <div class="medium-3 hide-for-small columns"><figure class="th"><a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {the_post_thumbnail('large'); } ?></a></figure></div>
-            <div class="medium-9 columns"><?php the_excerpt(); ?></div>
+            <?php if ( has_post_thumbnail() ) { ?>
+                <div class="medium-3 hide-for-small columns"><figure class="th"><a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {the_post_thumbnail('large'); } ?></a></figure></div>
+                <div class="medium-9 columns"><?php the_excerpt(); ?></div>
+            <?php } else { ?>
+                <div class="columns"><?php the_excerpt(); ?></div>
+            <?php } ?>
+
         </div>
         <p class="entry-tags"><?php the_tags(); ?></p>
     </div>

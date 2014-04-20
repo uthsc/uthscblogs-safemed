6c6740db270db2af1e146833916b1a97cee193d4
gpspake <gpspake@gmail.com>
2014-04-20 00:12:53 -0500

    Remove entry meta from pages

--- page.php
+++ page.php
@@ -8,7 +8,6 @@
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php reverie_entry_meta(); ?>
 			</header>
 			<div class="entry-content">
 				<?php the_content(); ?>

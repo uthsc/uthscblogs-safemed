e4425b46dea0c0e2e4ea5e2f7802ed70c29b5171
gpspake <gpspake@gmail.com>
2014-04-26 18:02:51 -0500

    Add off canvas navigation
    Moved modified header and footer from child theme to parent

--- footer.php
+++ footer.php
@@ -1,32 +1,40 @@
-	</div><!-- Row End -->
+</div><!-- Row End -->
 </div><!-- Container End -->
 
+
 <div class="full-width footer-widget">
-	<div class="row">
-		<?php dynamic_sidebar("Footer"); ?>
-	</div>
+    <div class="row">
+        <?php dynamic_sidebar("Footer"); ?>
+    </div>
 </div>
 
 <footer class="full-width" role="contentinfo">
-	<div class="row">
-		<div class="large-12 columns">
-			<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
-		</div>
-	</div>
-	<div class="row love-reverie">
-		<div class="large-12 columns">
-			<p>&copy; <?php echo date('Y'); ?> <a href="http://uthsc.edu" rel="nofollow" title="University of Tennessee Health Science Center">University of Tennessee Health Science Center</a>.</p>
-		</div>
-	</div>
+    <div class="row">
+        <div class="large-12 columns">
+            <?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
+        </div>
+    </div>
+    <div class="row love-reverie">
+        <div class="large-12 columns">
+            <p>&copy; <?php echo date('Y'); ?> <a href="http://uthsc.edu" rel="nofollow" title="University of Tennessee Health Science Center">University of Tennessee Health Science Center</a>.</p>
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
+</section>
+
+
+<a class="exit-off-canvas"></a>
+</div><!-- Inner Wrap End -->
+</div><!-- Off Canvas Wrap End -->
+
 
 <script>
-	(function($) {
-		$(document).foundation();
-	})(jQuery);
+    (function($) {
+        $(document).foundation();
+    })(jQuery);
 </script>
-	
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,34 +6,34 @@
 <!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?> > <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
 
-	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+    <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 
-	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
+    <!-- Mobile viewport optimized: j.mp/bplateviewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-	<!-- Favicon and Feed -->
-	<link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+    <!-- Favicon and Feed -->
+    <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
 
-	<!--  iPhone Web App Home Screen Icon -->
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-ipad.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-retina.png" />
-	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon.png" />
+    <!--  iPhone Web App Home Screen Icon -->
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-ipad.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-retina.png" />
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon.png" />
 
-	<!-- Enable Startup Image for iOS Home Screen Web App -->
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/mobile-load.png" />
+    <!-- Enable Startup Image for iOS Home Screen Web App -->
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/mobile-load.png" />
 
-	<!-- Startup Image iPad Landscape (748x1024) -->
-	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" />
-	<!-- Startup Image iPad Portrait (768x1004) -->
-	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
-	<!-- Startup Image iPhone (320x460) -->
-	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load.png" media="screen and (max-device-width: 320px)" />
+    <!-- Startup Image iPad Landscape (748x1024) -->
+    <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" />
+    <!-- Startup Image iPad Portrait (768x1004) -->
+    <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
+    <!-- Startup Image iPhone (320x460) -->
+    <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load.png" media="screen and (max-device-width: 320px)" />
 
-<?php wp_head(); ?>
+    <?php wp_head(); ?>
 
 </head>
 
@@ -47,71 +47,89 @@
         </div>
     </div>
 
-<!--    <div class="row hide-for-large-up search-form">
-        <div class="columns">
-            <?php /*get_search_form() */?>
-        </div>
-    </div>
--->
 </section>
 
-<header class="contain-to-grid">
-	<!-- Starting the Top-Bar -->
-	<nav class="top-bar" data-topbar>
-	    <ul class="title-area">
-	        <li class="name">
 
-                <div class="row hide-for-large-up search-form">
-                    <div class="small-9 medium-5 columns">
-                        <?php get_search_form() ?>
-                    </div>
-                </div>
 
-	        </li>
-			<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-	    </ul>
-	    <section class="top-bar-section">
-	    <?php
-	        wp_nav_menu( array(
-	            'theme_location' => 'primary',
-	            'container' => false,
-	            'depth' => 0,
-	            'items_wrap' => '<ul class="left">%3$s</ul>',
-	            'fallback_cb' => 'reverie_menu_fallback', // workaround to show a message to set up a menu
-	            'walker' => new reverie_walker( array(
-	                'in_top_bar' => true,
-	                'item_type' => 'li',
-	                'menu_type' => 'main-menu'
-	            ) ),
-	        ) );
-	    ?>
+<div class="off-canvas-wrap" data-offcanvas>
+    <div class="inner-wrap">
+        <nav class="tab-bar hide-for-large-up contain-to-grid">
+            <section class="left-small">
+                <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+            </section>
 
-	    <?php
-	    	// Uncomment the following to enable the right menu (additional menu)
-
-	        /*wp_nav_menu( array(
-	            'theme_location' => 'additional',
-	            'container' => false,
-	            'depth' => 0,
-	            'items_wrap' => '<ul class="right">%3$s</ul>',
-	            'walker' => new reverie_walker( array(
-	                'in_top_bar' => true,
-	                'item_type' => 'li',
-	                'menu_type' => 'main-menu'
-	            ) ),
-	        )
-            );*/
-
-	    ?>
-	    </section>
-        <section class="top-bar-section right show-for-large-up">
+            <section class="right tap-bar-section right ">
                 <?php get_search_form() ?>
-        </section>
-	</nav>
-	<!-- End of Top-Bar -->
-</header>
+            </section>
+        </nav>
 
-<!-- Start the main container -->
-<div class="container" role="document">
-	<div class="row content-area">
+        <aside class="left-off-canvas-menu">
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => 'off-canvas',
+                'container' => false,
+                'depth' => 0,
+                'items_wrap' => '<ul class="off-canvas-list">%3$s</ul>',
+                'fallback_cb' => 'reverie_menu_fallback', // workaround to show a message to set up a menu
+                'walker' => new reverie_walker( array(
+                        'in_top_bar' => true,
+                        'item_type' => 'li',
+                        'menu_type' => 'main-menu'
+                    ) ),
+            ) );
+            ?>
+
+            <!--<ul class="off-canvas-list">
+                <li><label>Foundation</label></li>
+                <li><a href="#">The Psychohistorians</a></li>
+                <li><a href="#">...</a></li>
+            </ul>-->
+        </aside>
+
+
+        <section class="main-section">
+
+            <header class="contain-to-grid show-for-large-up sticky">
+                <!-- Starting the Top-Bar -->
+                <nav class="top-bar" data-topbar>
+                    <ul class="title-area">
+                        <li class="name">
+
+                            <div class="row hide-for-large-up search-form">
+                                <div class="small-9 medium-5 columns">
+                                    <?php get_search_form() ?>
+                                </div>
+                            </div>
+
+                        </li>
+                        <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+                        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+                    </ul>
+                    <section class="top-bar-section">
+                        <?php
+                        wp_nav_menu( array(
+                            'theme_location' => 'primary',
+                            'container' => false,
+                            'depth' => 0,
+                            'items_wrap' => '<ul class="left">%3$s</ul>',
+                            'fallback_cb' => 'reverie_menu_fallback', // workaround to show a message to set up a menu
+                            'walker' => new reverie_walker( array(
+                                    'in_top_bar' => true,
+                                    'item_type' => 'li',
+                                    'menu_type' => 'main-menu'
+                                ) ),
+                        ) );
+                        ?>
+
+                    </section>
+                    <section class="top-bar-section right show-for-large-up">
+                        <?php get_search_form() ?>
+                    </section>
+                </nav>
+                <!-- End of Top-Bar -->
+            </header>
+
+
+            <!-- Start the main container -->
+            <div class="container" role="document">
+                <div class="row content-area">

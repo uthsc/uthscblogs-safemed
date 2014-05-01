367b3a16ea6755ce6893ea85d4f00be1e308868e
gpspake <gpspake@gmail.com>
2014-05-01 14:39:36 -0500

    Change apple touch icon

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
     <!--  iPhone Web App Home Screen Icon -->
     <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-ipad.png" />
     <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-retina.png" />
-    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon.png" />
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/devices/uthsc-icon.png" />
 
     <!-- Enable Startup Image for iOS Home Screen Web App -->
     <meta name="apple-mobile-web-app-capable" content="yes" />

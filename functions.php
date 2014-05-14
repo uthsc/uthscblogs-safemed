71990499c9ab19e81684f2dbb9e5ee86b733e26e
gpspake <gpspake@gmail.com>
2014-05-14 09:56:24 -0500

    Footer widget markup

--- functions.php
+++ functions.php
@@ -96,10 +96,10 @@ $sidebars = array('Footer');
 foreach ($sidebars as $sidebar) {
     register_sidebar(array('name'=> $sidebar,
     	'id' => 'Footer',
-        'before_widget' => '<div class="large-3 columns"><article id="%1$s" class="panel widget %2$s">',
+        'before_widget' => '<div class="large-4 columns"><article id="%1$s" class="widget %2$s">',
         'after_widget' => '</article></div>',
         'before_title' => '<h4>',
-        'after_title' => '</h4>'
+        'after_title' => '</h4><hr>'
     ));
 }
 

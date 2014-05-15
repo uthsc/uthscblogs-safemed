fc827c810af63bbfdbbbe28dbcba3371512a371d
gpspake <gpspake@gmail.com>
2014-05-15 08:37:14 -0500

    Use post_type for search results

--- search.php
+++ search.php
@@ -9,7 +9,7 @@
 	
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php get_template_part( 'content', get_post_type() ); ?>
 		<?php endwhile; ?>
 		
 		<?php else : ?>

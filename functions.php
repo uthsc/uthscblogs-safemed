62fe65f0f9f6e603b4a1f2fbd9fe29b73cf9b5aa
gpspake <gpspake@gmail.com>
2014-05-22 15:55:07 -0500

    entry meta - email instead of author posts

--- functions.php
+++ functions.php
@@ -106,7 +106,7 @@ foreach ($sidebars as $sidebar) {
 // return entry meta information for posts, used by multiple loops, you can override this function by defining them first in your child theme's functions.php file
 if ( ! function_exists( 'reverie_entry_meta' ) ) {
     function reverie_entry_meta() {
-        echo '<span class="byline author">'. __('Written by', 'reverie') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .', </a></span>';
+        echo '<span class="byline author">'. __('Written by', 'reverie') .' <a href="'. 'mailto:' . get_the_author_meta( 'user_email', get_the_author_meta('ID') ) .'" rel="author" class="fn">'. get_the_author() .', </a></span>';
         echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. get_the_time('F jS, Y') .'</time>';
     }
 };

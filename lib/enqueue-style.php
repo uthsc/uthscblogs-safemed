88f7beb1e90feee563111d2a7df11d048b212dcd
gpspake <gpspake@gmail.com>
2014-07-28 13:58:45 -0500

    Import reverie stylesheet so we only need to load one

--- lib/enqueue-style.php
+++ lib/enqueue-style.php
@@ -10,7 +10,7 @@ if( ! function_exists( 'reverie_enqueue_style' ) ) {
 		//wp_register_style( 'reverie-foundation-stylesheet', get_template_directory_uri() . '/css/app.css', array(), '' );
 
 		// Register the main style
-		wp_register_style( 'reverie-stylesheet', get_template_directory_uri() . '/css/style.css', array(), '', 'all' );
+		//wp_register_style( 'reverie-stylesheet', get_template_directory_uri() . '/css/style.css', array(), '', 'all' );
 
         // Register the uthsc stylesheet
         wp_register_style( 'uthsc-stylesheet', get_template_directory_uri() . '/css/uthsc.css', array(), '', 'all' );
@@ -18,7 +18,7 @@ if( ! function_exists( 'reverie_enqueue_style' ) ) {
         // Register the uthsc stylesheet
         wp_register_style( 'ie-stylesheet', get_template_directory_uri() . '/css/ie.css', array(), '', 'all' );
 
-		wp_enqueue_style( 'reverie-foundation-stylesheet' );
+		//wp_enqueue_style( 'reverie-foundation-stylesheet' );
 		wp_enqueue_style( 'reverie-stylesheet' );
         wp_enqueue_style( 'uthsc-stylesheet' );
         wp_enqueue_style( 'ie-stylesheet' );

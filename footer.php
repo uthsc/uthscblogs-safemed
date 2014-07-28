1980f600de31cf8a53b462f7118d593da3ee22e5
gpspake <gpspake@gmail.com>
2014-07-28 13:55:10 -0500

    Improve social links in footer

--- footer.php
+++ footer.php
@@ -1,80 +1,55 @@
 </div><!-- Row End -->
 </div><!-- Container End -->
 
-
-<div class="full-width footer-widget">
-    <div class="row">
-
-    </div>
-</div>
+<div class="full-width footer-widget"><div class="row"></div></div>
 
 <footer class="full-width" role="contentinfo">
     <div class="row">
-
         <?php dynamic_sidebar("Footer"); ?>
     </div>
     <div class="uthsc-copy">
-    <div class="row">
-        <div class="large-6 columns">
-            <p>&copy; <?php echo date('Y'); ?> <a href="http://uthsc.edu" rel="nofollow" title="University of Tennessee Health Science Center">University of Tennessee Health Science Center</a>
-                Memphis, Tennessee 38163 · Main Phone Number: 901-448-5500<br>TDD (Telephone Device for the Deaf Line) Line: 901-448-7382</p>
-        </div>
-        <div class="large-6 columns">
-            <div class="fs_icons">
-
-                <div class="desktop-social-icons show-for-medium-up">
-                    <div class="footer_social_facebook fs_icon_web">
-                        <a href="https://www.facebook.com/uthsc"></a>
-                    </div>
-                    <div class="footer_social_twitter fs_icon_web">
-                        <a href="https://twitter.com/uthsc"></a>
-                    </div>
-                    <div class="footer_social_linkedin fs_icon_web">
-                        <a href="http://www.linkedin.com/company/university-of-tennessee-health-science-center"></a>
-                    </div>
-
-                    <div class="footer_social_youtube fs_icon_web">
-                        <a href="http://www.youtube.com/uthsc"></a>
-                    </div>
-                </div>
-
-
-
-                <div class="mobile-social-icons hide-for-medium-up">
-                    <div class="footer_social_facebook fs_icon_mobile">
-                        <a href="fb://profile?id=19251347752"></a>
-                    </div>
-                    <div class="footer_social_twitter fs_icon_mobile">
-                        <a href="twitter:///user?screen_name=uthsc"></a>
-                    </div>
-                    <div class="footer_social_linkedin fs_icon_mobile">
-                        <a href="linkedin://company/230771"></a>
-                    </div>
-                    <div class="footer_social_youtube fs_icon_mobile">
-                        <a href="http://youtube.com/uthsc"></a>
-                    </div>
-                </div>
-
-
-
-
-
-
-
+        <div class="row">
+            <div class="large-6 columns">
+                <p>&copy; <?php echo date('Y'); ?> <a href="http://uthsc.edu" rel="nofollow" title="University of Tennessee Health Science Center">University of Tennessee Health Science Center</a><br>
+                    Memphis, Tennessee 38163 · Main Phone Number: 901-448-5500<br>TDD (Telephone Device for the Deaf Line) Line: 901-448-7382</p>
             </div>
-        </div>
+            <div class="large-6 columns">
+                <div class="fs_icons">
+
+                    <div class="desktop-social-icons">
+                        <div class="footer_social_facebook fs_icon_web">
+                            <a href="https://www.facebook.com/uthsc"></a>
+                        </div>
+                        <div class="footer_social_twitter fs_icon_web">
+                            <a href="https://twitter.com/uthsc"></a>
+                        </div>
+
+                        <div class="footer_social_instagram fs_icon_web">
+                            <a href="http://instagram.com/uthsc"></a>
+                        </div>
+
+                        <div class="footer_social_linkedin fs_icon_web">
+                            <a href="http://www.linkedin.com/company/university-of-tennessee-health-science-center"></a>
+                        </div>
+
+                        <div class="footer_social_youtube fs_icon_web">
+                            <a href="http://www.youtube.com/uthsc"></a>
+                        </div>
+
+                        <div class="footer_social_pinterest fs_icon_web">
+                            <a href="http://www.pinterest.com/uthsc/"></a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
 </footer>
 
 <?php wp_footer(); ?>
-</section>
 
-
-<a class="exit-off-canvas"></a>
 </div><!-- Inner Wrap End -->
-</div><!-- Off Canvas Wrap End -->
-
 
 <script>
     (function($) {

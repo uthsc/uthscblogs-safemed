0fef04184207cf386032e631c643b436a2ef5fb2
gpspake <gpspake@gmail.com>
2014-08-04 16:31:42 -0500

    Header Improvements
    Change site title font, add ie9 bg and cleanup

--- header.php
+++ header.php
@@ -1,43 +1,35 @@
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js ie6 lt-ie9 oldie" <?php language_attributes(); ?> > <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 lt-ie9 oldie" <?php language_attributes(); ?> > <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 lt-ie9 oldie" <?php language_attributes(); ?> "> <![endif]-->
+<!--[if IE 7]><html class="no-js ie7 lt-ie9 oldie" <?php language_attributes(); ?> > <![endif]-->
+<!--[if IE 8]><html class="no-js ie8 lt-ie9 oldie" <?php language_attributes(); ?> "> <![endif]-->
+<!--[if IE 9]><html class="no-js ie9" <?php language_attributes(); ?> "> <![endif]-->
+
 <!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?> > <!--<![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" <?php language_attributes(); ?> > <!--<![endif]-->
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-
     <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
-
     <!-- Mobile viewport optimized: j.mp/bplateviewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
     <!-- Favicon and Feed -->
     <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
-
     <!--  iPhone Web App Home Screen Icon -->
     <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-ipad.png" />
     <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-retina.png" />
     <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/devices/uthsc-icon.png" />
-
     <!-- Enable Startup Image for iOS Home Screen Web App -->
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/mobile-load.png" />
-
     <!-- Startup Image iPad Landscape (748x1024) -->
     <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" />
     <!-- Startup Image iPad Portrait (768x1004) -->
     <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
     <!-- Startup Image iPhone (320x460) -->
     <link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load.png" media="screen and (max-device-width: 320px)" />
-
     <?php wp_head(); ?>
 </head>
-
 <body <?php body_class('antialiased'); ?>>
-
 <section id="banner">
     <div class="row brand-text">
         <div id="brand" class="columns">
@@ -46,9 +38,7 @@
         </div>
     </div>
 </section>
-
     <div class="inner-wrap">
-
             <header class="contain-to-grid sticky">
                 <!-- Starting the Top-Bar -->
                 <nav class="top-bar" data-topbar data-options="scrolltop:false;">
@@ -62,7 +52,6 @@
                             </div>
 
                         </li>
-                        <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
                         <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
                     </ul>
                     <section class="top-bar-section">
@@ -80,7 +69,6 @@
                                 ) ),
                         ) );
                         ?>
-
                     </section>
                     <section class="top-bar-section right show-for-large-up">
                         <?php get_search_form() ?>
@@ -88,7 +76,6 @@
                 </nav>
                 <!-- End of Top-Bar -->
             </header>
-
             <!-- Start the main container -->
             <div class="container" role="document">
                 <div class="row content-area">

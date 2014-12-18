f76f2300d87fd10616e35875000792bc9ed60622
gpspake <gpspake@gmail.com>
2014-12-18 12:47:09 -0600

    Remove border from search form

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
-    <input type="text" class="radius" value="" name="s" id="s" placeholder="<?php esc_attr_e('Search', 'reverie'); ?>">
+    <input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e('Search', 'reverie'); ?>">
 </form>

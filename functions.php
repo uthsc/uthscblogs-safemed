264506bc10112eedb03276253eae8170dfcab4fe
gpspake <gpspake@gmail.com>
2015-05-22 09:56:44 -0500

    update thumb size

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ if( ! function_exists( 'reverie_theme_support' ) ) {
 
         // Add post thumbnail supports. http://codex.wordpress.org/Post_Thumbnails
         add_theme_support('post-thumbnails');
-        // set_post_thumbnail_size(150, 150, false);
+        set_post_thumbnail_size(300, 300);
         add_image_size('fd-lrg', 1024, 99999);
         add_image_size('fd-med', 768, 99999);
         add_image_size('fd-sm', 320, 9999);

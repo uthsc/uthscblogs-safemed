764cbab58bf04250a32248999527006e99093629
Nic Winn <nic.liamg@gmail.com>
2016-02-24 09:55:44 -0600

    replaced footer content

--- footer.php
+++ footer.php
@@ -9,126 +9,67 @@
             </div>
         </div>
 
-        <!--Bottom Footer-->
-        <div class="uthsc-footer-bottom full-width">
-            <div class="row collapse">
-                <div class="columns text-center">
-                    <br/>
-
-                    <p>&copy; 2015 The University of Tennessee<br/>Health Science Center<br/>
-                        Memphis, Tennessee 38163<br/>
-                        Main: (901) 448-5500<br/>
-                        TDD: (901) 448-7382</p>
-                    <br/>
-                    <a href="mailto:webmaster@uthsc.edu" class="button radius secondary tiny"><i
-                            class="fa fa-envelope fa-3x two-line-button-icon"></i><span class="two-line-button-text">Email<br/>Webmaster</span></a>
-
-                    <a href="/password/" class="button radius secondary tiny"><i
-                            class="fa fa-key fa-3x two-line-button-icon"></i><span class="two-line-button-text">Edit<br/>Password</span></a>
-                    <br/>
-
-                    <div class="social-icon">
-                        <a href="http://facebook.com/uthsc"><i class="fa fa-facebook fa-2x fa-inverse"></i></a>
-                        <a href="http://twitter.com/uthsc"><i class="fa fa-twitter fa-2x fa-inverse"></i></a>
-                        <a href="http://instagram.com/uthsc"><i class="fa fa-instagram fa-2x fa-inverse"></i></a>
-                        <a href="http://linkedin.com/company/university-of-tennessee-health-science-center"><i
-                                class="fa fa-linkedin fa-2x fa-inverse"></i></a>&nbsp;
-                        <a href="http://pinterest.com/uthsc/"><i class="fa fa-youtube fa-2x fa-inverse"></i></a>
-                        <a href="http://youtube.com/user/uthsc"><i class="fa fa-pinterest fa-2x fa-inverse"></i></a>
-                    </div>
-                    <br/>
-
-                </div>
-            </div>
-
-            <div class="row collapse">
-                <div id="emergency-mobile" class="columns small-12 show-for-small">
-
-
-                    <h3 class="text-center"><i class="fa fa-phone fa-flip-horizontal fa-2x"></i> Emergency Numbers</h3>
-                    <a href="#" data-reveal-id="myModal1" class="button alert expand"><i class="fa fa-info-circle"></i> Medical/Behavioral</a>
-                    <div id="myModal1" class="reveal-modal text-center" data-reveal>
-                        <div class="call-buttons-box">
-                            <h2>Medical/Behavioral</h2>
-                            <hr />
-                            <h3 class="lead"><a class="button emergency" href="tel:911"><i class="fa fa-phone fa-2x fa-flip-horizontal fa-button"></i> 911</a></h3>
-                            <p>or Campus Police at</p>
-                            <h3><a class="button emergency" href="tel:(901) 448-4444"><i class="fa fa-phone fa-2x fa-flip-horizontal fa-button"></i> (901) 448-4444</a></h3>
-                        </div>
-                        <p><a href="#" data-reveal-id="myModal6" class="secondary button tiny expand"><i class="fa fa-chevron-up fa-2x fa-button"></i> Previous Emergency Number</a>
-                            <a href="#" data-reveal-id="myModal2" class="secondary button tiny expand"><i class="fa fa-chevron-down fa-2x fa-button"></i> Next Emergency Number&emsp;&emsp;</a></p>
-                        <a class="close-reveal-modal">&#215;</a>
-                    </div>
-                    <a href="#" data-reveal-id="myModal2" class="button alert expand"><i class="fa fa-info-circle"></i> Medical Urgent Care</a>
-                    <div id="myModal2" class="reveal-modal text-center" data-reveal>
-                        <div class="call-buttons-box">
-                            <h2>Medical Urgent Care</h2>
-                            <hr />
-                            <h3 class="lead"><a class="button emergency" href="tel:(901) 448-5630"><i class="fa fa-phone fa-2x fa-flip-horizontal fa-button"></i> (901) 448-5630</a></h3>
-                        </div>
-                        <p><a href="#" data-reveal-id="myModal1" class="secondary button tiny expand"><i class="fa fa-chevron-up fa-2x fa-button"></i> Previous Emergency Number</a>
-                            <a href="#" data-reveal-id="myModal3" class="secondary button tiny expand"><i class="fa fa-chevron-down fa-2x fa-button"></i> Next Emergency Number&emsp;&emsp;</a></p>
-                        <a class="close-reveal-modal">&#215;</a>
-                    </div>
-                    <a href="#" data-reveal-id="myModal3" class="button alert expand"><i class="fa fa-info-circle"></i> Behavioral Health Urgent Care</a>
-                    <div id="myModal3" class="reveal-modal text-center" data-reveal>
-                        <div class="call-buttons-box">
-                            <h2>Behavioral Health Urgent Care</h2>
-                            <hr />
-                            <h3 class="lead"><a class="button emergency" href="tel:(901) 448-5064"><i class="fa fa-phone fa-2x fa-flip-horizontal fa-button"></i> (901) 448-5064</a></h3>
-                        </div>
-                        <p><a href="#" data-reveal-id="myModal2" class="secondary button tiny expand"><i class="fa fa-chevron-up fa-2x fa-button"></i> Previous Emergency Number</a>
-                            <a href="#" data-reveal-id="myModal4" class="secondary button tiny expand"><i class="fa fa-chevron-down fa-2x fa-button"></i> Next Emergency Number&emsp;&emsp;</a></p>
-                        <a class="close-reveal-modal">&#215;</a>
-                    </div>
-                    <a href="#" data-reveal-id="myModal4" class="button alert expand"><i class="fa fa-info-circle"></i> Student Assistance Program</a>
-                    <div id="myModal4" class="reveal-modal text-center" data-reveal>
-                        <div class="call-buttons-box">
-                            <h2>Student Assistance Program (SAP)</h2>
-                            <hr />
-                            <h3 class="lead"><a class="button emergency" href="tel:800-327-2255"><i class="fa fa-phone fa-2x fa-flip-horizontal fa-button"></i> 800-327-2255<br />(Option 3)</a></h3>
-                        </div>
-                        <p><a href="#" data-reveal-id="myModal3" class="secondary button tiny expand"><i class="fa fa-chevron-up fa-2x fa-button"></i> Previous Emergency Number</a>
-                            <a href="#" data-reveal-id="myModal5" class="secondary button tiny expand"><i class="fa fa-chevron-down fa-2x fa-button"></i> Next Emergency Number&emsp;&emsp;</a></p>
-                        <a class="close-reveal-modal">&#215;</a>
-                    </div>
-                    <a href="#" data-reveal-id="myModal5" class="button alert expand"><i class="fa fa-info-circle"></i> Employee Assistance Program</a>
-                    <div id="myModal5" class="reveal-modal text-center" data-reveal>
-                        <div class="call-buttons-box">
-                            <h2>Employee Assistance Program (EAP)</h2>
-                            <hr />
-                            <h3 class="lead"><a class="button emergency" href="tel:855-437-3486"><i class="fa fa-phone fa-2x fa-flip-horizontal fa-button"></i> 855-437-3486</a></h3>
-                        </div>
-                        <p><a href="#" data-reveal-id="myModal4" class="secondary button tiny expand"><i class="fa fa-chevron-up fa-2x fa-button"></i> Previous Emergency Number</a>
-                            <a href="#" data-reveal-id="myModal6" class="secondary button tiny expand"><i class="fa fa-chevron-down fa-2x fa-button"></i> Next Emergency Number&emsp;&emsp;</a></p>
-                        <a class="close-reveal-modal">&#215;</a>
-                    </div>
-                    <a href="#" data-reveal-id="myModal6" class="button alert expand"><i class="fa fa-info-circle"></i> After Hours Medical/Behavioral</a>
-                    <div id="myModal6" class="reveal-modal text-center" data-reveal>
-                        <div class="call-buttons-box">
-                            <h2>After Hours Medical/Behavioral<br />Available (24/7)</h2>
-                            <hr />
-                            <h3 class="lead"><a class="button emergency" href="tel:(901) 541-5654"><i class="fa fa-phone fa-2x fa-flip-horizontal fa-button"></i> (901) 541-5654</a></h3>
-                        </div>
-                        <p><a href="#" data-reveal-id="myModal5" class="secondary button tiny expand"><i class="fa fa-chevron-up fa-2x fa-button"></i> Previous Emergency Number</a>
-                            <a href="#" data-reveal-id="myModal1" class="secondary button tiny expand"><i class="fa fa-chevron-down fa-2x fa-button"></i> Next Emergency Number&emsp;&emsp;</a></p>
-                        <a class="close-reveal-modal">&#215;</a>
-                    </div>
-                </div>
-                <div id="emergency-desktop" class="medium-5 medium-centered columns hide-for-small">
-                    <div class="row">
-                        <a href="/univheal/emergencies.php" class="button alert radius expand">
-
-                            <div class="columns large-5 medium-4 text-right emergency-phone-icon"><i class="fa fa-phone fa-flip-horizontal fa-3x"></i></div>
-                            <div class="columns large-7 medium-8 text-left"><h4>Emergency<br/>Numbers</h4></div>
-                            <div class="columns small-12">Call 911 or Campus Police at (901) 448-4444<br/>
-                                More Emergency Information</div>
-
-                        </a>
-                    </div>
-                </div>
+    <div class="sub-tagline-box contain-to-grid">
+        <div class="row">
+            <div class="columns text-center">
+                <p class="sub-tagline">A joint venture between University of Tennessee Health Science Center (UTHSC) and Methodist LeBonheur HealthCare</p>
             </div>
         </div>
+    </div>
+    <div class="row full">
+        <div class="columns text-center">
+            <h3>Call <i class="fa fa-phone"></i> UTHSC Center for Health Systems Improvement: <span class="pnone-number">(901) 448-2475</span></h3>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="footer contain-to-grid">
+        <footer class="row text-center">
+            <div class="large-12 columns large-centered">
+                <div class="row" data-equalizer="data-equalizer">
+                    <div class="columns large-2 panel radius methodist-panel logo-panel show-for-large-up" data-equalizer-watch="data-equalizer-watch">
+                    </div>
+                    <div class="columns large-8 small-centered medium-uncentered large-uncentered" data-equalizer-watch="data-equalizer-watch">
+                        <div class="social-icon">
+                            <a href="http://facebook.com/#"><i class="fa fa-facebook fa-2x fa-inverse"></i></a>
+                            <a href="http://twitter.com/#"><i class="fa fa-twitter fa-2x fa-inverse"></i></a>
+                            <a href="http://youtube.com/user/#"><i class="fa fa-youtube fa-2x fa-inverse"></i></a>
+                            <p>&copy; <?php echo date("Y") ?> The University of Tennessee Health Science Center - Center for Health System Improvement
+                                <br />Methodist Le Bonheur Healthcare
+                            </p>
+                        </div>
+                    </div>
+                    <div class="columns large-2 panel radius uthsc-panel logo-panel show-for-large-up" data-equalizer-watch="data-equalizer-watch">
+                    </div>
+                </div>
+            </div>
+            <div class="columns logo-panel-group">
+                <div class="row hide-for-large-up collapse" data-equalizer="data-equalizer">
+                    <div class="columns small-12 small-centered medium-uncentered medium-6 panel radius methodist-panel logo-panel" data-equalizer-watch="data-equalizer-watch">
+                    </div>
+                    <div class="columns small-12 small-centered medium-uncentered medium-6 panel radius uthsc-panel logo-panel" data-equalizer-watch="data-equalizer-watch">
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script>
+
+        $(document).ready(function() {
+            $('.link-pdf').attr('title', "Link to a PDF").attr('aria-label', "PDF Icon");
+            $('a', '.links-pdf').attr('title', "Link to a PDF").attr('aria-label', "PDF Icon");
+            $('.link-external').attr('title', "External Link").attr('aria-label', "External Link Icon");
+            $('a', '.links-external').attr('title', "External Link").attr('aria-label', "External Link Icon");
+            $('.link-word').attr('title', "Link to a Word Document").attr('aria-label', "Microsoft Word Icon");
+            $('a', '.links-word').attr('title', "Link to a Word Document").attr('aria-label', "Microsoft Word Icon");
+            $('.link-excel').attr('title', "Link to a Excel Document").attr('aria-label', "Microsoft Excel Icon");
+            $('a', '.links-excel').attr('title', "Link to a Excel Document").attr('aria-label', "Microsoft Excel Icon");
+            $('.link-ppt').attr('title', "Link to a PowerPoint Document").attr('aria-label', "Microsoft PowerPoint Icon");
+            $('a', '.links-excel').attr('title', "Link to a PowerPoint Document").attr('aria-label', "Microsoft PowerPoint Icon");
+        });
+    </script>
 
     <?php wp_footer(); ?>
 

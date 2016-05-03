9db34d132be665750d2340bf18eb2f3e5d9e41d2
Nic Winn <nic.liamg@gmail.com>
2016-05-03 16:32:08 -0500

    switched to google tag manager for analytics

--- header.php
+++ header.php
@@ -24,23 +24,7 @@
     <meta property="og:url" content="http://safemed.org"/>
     <meta property="og:description"
           content="SafeMed: A joint venture between University of Tennessee Health Science Center (UTHSC) and Methodist LeBonheur HealthCare"/>
-    <script>
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-        ga('create', 'UA-71447945-1', 'auto');
-        ga('send', 'pageview');
-
-    </script>
     <!-- Favicon and Feed -->
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed"
           href="<?php echo home_url(); ?>/feed/">
@@ -73,6 +57,16 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('antialiased'); ?>>
+
+<!--    Google Analytics via Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KL76K8"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KL76K8');</script>
+<!--/   Google Analytics via Tag Manager -->
 
 <div id="sticky-footer-root">
     <section id="banner" class="hero">
